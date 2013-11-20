9d8899ab6fe522e99a8a5f1287fbd55499331a5a
Marko Lalic <marko.lalic@gmail.com>
2013-11-20 19:19:27 +0100

    Implement a very simple test client.
    The client uses the KVStore API directly (no CLI or commands)
    in order to establish a connection with the KV store server,
    and write/read a key/value pair.
    
    This client is here only for the purpose of running the simplest
    functionality checks and that is why it's found in a separate
    temporary branch.

--- BasicStorageServer/src/app_kvClient/KVClient.java
+++ BasicStorageServer/src/app_kvClient/KVClient.java
@@ -1,7 +1,53 @@
 package app_kvClient;
 
+import client.KVStore;
+
 
 public class KVClient {
 
+    private final static String PROMPT = "EchoClient> ";
+    
+
+    public static void main(String[] args) {
+
+    	KVStore store = new KVStore("localhost", 8000);
+    	
+    	try {
+			store.connect();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	try {
+			store.put("key", "value");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	try {
+			System.out.println(store.get("key").getValue());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	try {
+			store.put("key", "new-value");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    	try {
+			System.out.println(store.get("key").getValue());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+    	
+    	
+    }
 
 }
