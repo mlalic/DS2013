b89ac9dd808cd3aa44174748ae37346cae4b3e12
Marko Lalic <marko.lalic@gmail.com>
2013-11-22 09:28:10 +0100

    Do not output the log to the console - only the log file.

--- BasicStorageServer/src/app_kvServer/KVServer.java
+++ BasicStorageServer/src/app_kvServer/KVServer.java
@@ -98,19 +98,24 @@ public class KVServer extends Thread {
     	try {
 			new LogSetup("logs/server/server.log", Level.ALL);
 			if(args.length != 1) {
+                System.out.println("Error! Invalid number of arguments!");
+				System.out.println("Usage: Server <port>!");
+				
 				logger.error("Error! Invalid number of arguments!");
-				logger.error("Usage: Server <port>!");
 			} else {
 				int port = Integer.parseInt(args[0]);
 				new KVServer(port).start();
 			}
 		} catch (IOException e) {
+			System.out.println("Error! Unable to initialize logger!");
 			logger.error("Error! Unable to initialize logger!");
 			e.printStackTrace();
 			System.exit(1);
 		} catch (NumberFormatException nfe) {
+			System.out.println("Error! Invalid argument <port>! Not a number!");
+			System.out.println("Usage: Server <port>!");
+
 			logger.error("Error! Invalid argument <port>! Not a number!");
-			logger.error("Usage: Server <port>!");
 			System.exit(1);
 		}
     }
