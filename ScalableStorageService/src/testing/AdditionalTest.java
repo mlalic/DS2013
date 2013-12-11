5f8604ce9d0175156f7e7816676562b9de9fe9d9
Marko Lalic <marko.lalic@gmail.com>
2013-12-11 17:46:04 +0100

    Remove the KeyRange class.
    This class is no longer necessary since the alternative way of
    finding servers responsible for a key has been implemented.

--- ScalableStorageService/src/testing/AdditionalTest.java
+++ ScalableStorageService/src/testing/AdditionalTest.java
@@ -8,7 +8,6 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 import common.metadata.KeyHasher;
-import common.metadata.KeyRange;
 import common.metadata.Md5Hasher;
 import common.metadata.MetaData;
 import common.metadata.MetaDataTransport;
@@ -16,57 +15,6 @@ import common.metadata.ServerNode;
 
 public class AdditionalTest extends TestCase {
 
-	/**
-	 * Tests that the {@link KeyRange#isInRange(String) isInRange} method
-	 * runs correctly for "normal" ranges (where end > start).
-	 */
-	@Test
-	public void testInRange() {
-		final String start = "00";
-		final String end = "AA";
-		KeyRange range = new KeyRange(start, end);
-		
-		// A value obviously in the range
-		assertTrue(range.isInRange("01"));
-		// Start is included in the range
-		assertTrue(range.isInRange(start));
-		// End is not included in the range
-		assertFalse(range.isInRange(end));
-		// A value completely outside of the range
-		assertFalse(range.isInRange("FF"));
-		assertFalse(range.isInRange("AB"));
-	}
-
-	/**
-	 * Tests that the {@link KeyRange#isInRange(String) isInRange} method
-	 * runs correctly for ranges which "wrap around" the hash circle, i.e.
-	 * end < start.
-	 */
-	@Test
-	public void testInRangeBorder() {
-		final String start = "CD";
-		final String end = "10";
-		
-		KeyRange range = new KeyRange(start, end);
-		
-		// The start is considered in the range
-		assertTrue(range.isInRange(start));
-		// A value in the range
-		assertTrue(range.isInRange("CE"));
-		// The largest key value is also in the range
-		assertTrue(range.isInRange("FF"));
-		// ..as is the smallest
-		assertTrue(range.isInRange("00"));
-		// A value strictly less than the end is in the range
-		assertTrue(range.isInRange("09"));
-		// The end is considered outside of the range
-		assertFalse(range.isInRange(end));
-		// Values higher than the range's end are not in it.
-		assertFalse(range.isInRange("11"));
-		// A value strictly less than the range's beginning is not in it.
-		assertFalse(range.isInRange("CC"));
-	}
-	
 	@Test
 	public void testMetaData() {
 		MetaData metaData = new MetaData();
