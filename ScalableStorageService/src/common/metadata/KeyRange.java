de094095635ffa6d3b7a16f34b9832628dda6895
Marko Lalic <marko.lalic@gmail.com>
2013-12-11 14:36:28 +0100

    Implement a KeyRange class.
    This class provides the necessary functionality to represent
    key ranges. It exposes a convenient method to check whether
    a particular key is a part of the range.

---
+++ ScalableStorageService/src/common/metadata/KeyRange.java
@@ -0,0 +1,62 @@
+package common.metadata;
+
+/**
+ * The key range is represented by a half-open interval, namely
+ * [start, end). 
+ *
+ */
+public class KeyRange {
+	
+	private String start;
+	private String end;
+	
+	public KeyRange(String start, String end) {
+		if (start.length() != end.length()) {
+			throw new IllegalArgumentException(
+					"The strings representing the ranges need to be of the same length");
+		}
+		this.start = start;
+		this.end = end;
+	}
+	
+	public boolean isInRange(String key) {
+		// If two hex numbers are represented and strings,
+		// doing a lexicographic comparison is equivalent to
+		// a number magnitude comparison iff the two strings
+		// have an equal number of digits.
+		if (key.length() != start.length()) {
+			throw new IllegalArgumentException(
+					"The length of the key needs to be the same as the length of the start and end parameters of the range");
+		}
+
+		// start is included
+		final boolean greaterThanStart = start.compareTo(key) <= 0;
+		// end is excluded
+		final boolean lessThanEnd = key.compareTo(end) < 0;
+		
+		if (end.compareTo(start) < 0) {
+			// Special case when the range "wraps-around" the ring.
+			// It means that the start of the range is found at a position
+			// with a higher magnitude than the end of the range.
+			
+			// The key is found in this range when it is any of the numbers higher than the
+			// start (since the highest possible one is in the range, taking the wrap-around
+			// assumption).
+			// The key is also found in the range when it is any of the numbers smaller
+			// than the end, since the smallest one is also in the range given the assumption.
+			return greaterThanStart || lessThanEnd;
+		}		
+
+		return greaterThanStart && lessThanEnd;
+	}
+	
+	public String getStart() {
+		return start;
+	}
+
+	public String getEnd() {
+		return end;
+	}
+
+
+}
--- ScalableStorageService/src/testing/AdditionalTest.java
+++ ScalableStorageService/src/testing/AdditionalTest.java
@@ -2,14 +2,62 @@ package testing;
 
 import org.junit.Test;
 
+
+import common.metadata.KeyRange;
 import junit.framework.TestCase;
 
 public class AdditionalTest extends TestCase {
-	
-	// TODO add your test cases, at least 3
-	
+
+	/**
+	 * Tests that the {@link KeyRange#isInRange(String) isInRange} method
+	 * runs correctly for "normal" ranges (where end > start).
+	 */
 	@Test
-	public void testStub() {
-		assertTrue(true);
+	public void testInRange() {
+		final String start = "00";
+		final String end = "AA";
+		KeyRange range = new KeyRange(start, end);
+		
+		// A value obviously in the range
+		assertTrue(range.isInRange("01"));
+		// Start is included in the range
+		assertTrue(range.isInRange(start));
+		// End is not included in the range
+		assertFalse(range.isInRange(end));
+		// A value completely outside of the range
+		assertFalse(range.isInRange("FF"));
+		assertFalse(range.isInRange("AB"));
+	}
+
+	/**
+	 * Tests that the {@link KeyRange#isInRange(String) isInRange} method
+	 * runs correctly for ranges which "wrap around" the hash circle, i.e.
+	 * end < start.
+	 */
+	@Test
+	public void testInRangeBorder() {
+		final String start = "CD";
+		final String end = "10";
+		
+		KeyRange range = new KeyRange(start, end);
+		
+		// The start is considered in the range
+		assertTrue(range.isInRange(start));
+		// A value in the range
+		assertTrue(range.isInRange("CE"));
+		// The largest key value is also in the range
+		assertTrue(range.isInRange("FF"));
+		// ..as is the smallest
+		assertTrue(range.isInRange("00"));
+		// A value strictly less than the end is in the range
+		assertTrue(range.isInRange("09"));
+		// The end is considered outside of the range
+		assertFalse(range.isInRange(end));
+		// Values higher than the range's end are not in it.
+		assertFalse(range.isInRange("11"));
+		// A value strictly less than the range's beginning is not in it.
+		assertFalse(range.isInRange("CC"));
+	}
+	
 	}
 }
