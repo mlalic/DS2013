1dc4b20fa95d41e54d0007da6d3dc79538bc1be6
Balsa Bojic <balsha.bojic@gmail.com>
2013-12-13 21:17:13 +0100

    As we said that we will have only one message for communication between all nodes. Therefore I have added some additional statuses so we can noramly parse messages form ECS. Thanks to these statuses, server is making special commands for communicating.

--- ScalableStorageService/src/common/messages/KVMessage.java
+++ ScalableStorageService/src/common/messages/KVMessage.java
@@ -12,9 +12,27 @@ public interface KVMessage {
     	PUT_ERROR,       		/* Put - request not successful */
     	DELETE_SUCCESS,  		/* Delete - request successful */
     	DELETE_ERROR,     		/* Delete - request successful */
+    	
     	SERVER_STOPPED,         /* Server is stopped, no requests are processed */
     	SERVER_WRITE_LOCK,      /* Server locked for out, only get possible */
-    	SERVER_NOT_RESPONSIBLE  /* Request not successful, server not responsible for key */
+    	SERVER_NOT_RESPONSIBLE,  /* Request not successful, server not responsible for key */
+    	
+    	COMM_ERROR, 			/* Sever sends error (communication error) message */
+    	INIT_SEREVER,			/* ECS initializes server and sends metadata */
+    	INIT_ACK,				/* Server sends init acknowledgement to ECS */
+    	SHUT_DOWN,				/* ECS requests server to shut down */
+    	SHUT_DOWN_ACK,			/* Server sends shut down acknowledgement to ECS*/	
+    	START_SERVER, 			/* ECS requests server to start working  */
+    	START_ACK,				/* Server sends start acknowledgement to ECS */
+    	STOP_SERVER,            /* ECS requests server to stop working  */
+    	STOP_ACK,				/* Server sends stop acknowledgement to ECS */
+    	LOCK_WRITE,				/* ECS locks writing on server */	
+    	LOCK_WRITE_ACK,			/* Server sends lock write acknowledgement to ECS */
+    	UNLOCK_WRITE,			/* ECS unlocks writing on server */
+    	UNLOCK_WRITE_ACK,		/* Server sends unlock write acknowledgement to ECS */
+    	MOVE_DATA,				/* ECS requests server to move data to other server */
+    	UPDATE_METADATA,		/* ECS is sending new metadata to server */
+    	UPDATE_METADATA_ACK		/* Server sends meta data update acknowledgement to ECS*/
 }
 
 	/**
