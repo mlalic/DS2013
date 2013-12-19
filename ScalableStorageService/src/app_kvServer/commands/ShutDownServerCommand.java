ec73b21c55534079d8731e739c71f50037935480
Marko Lalic <marko.lalic@gmail.com>
2013-12-19 17:50:26 +0100

    Make the server-side shut down command exit the process.
    The shutdown command (as per the Milestone description) should
    halt the server-side process.

--- ScalableStorageService/src/app_kvServer/commands/ShutDownServerCommand.java
+++ ScalableStorageService/src/app_kvServer/commands/ShutDownServerCommand.java
@@ -2,10 +2,9 @@ package app_kvServer.commands;
 
 import org.apache.log4j.Logger;
 
-import app_kvServer.KVServer.ServerStatusType;
 import app_kvServer.ServerContext;
+
 import common.messages.KVMessage;
-import common.messages.KVMessageImpl;
 
 public class ShutDownServerCommand extends ServerCommand {
 
@@ -17,18 +16,14 @@ public class ShutDownServerCommand extends ServerCommand {
 
 	@Override
 	public KVMessage execute() {
-		serverContext.setServerStatus(ServerStatusType.IDLE);
-		responseMessage = new KVMessageImpl(KVMessage.StatusType.ACK, key, value);
-		logger.info("Server is successesfully shuted down. Now server is in the idle state (waiting to be added in ring again).");
-		return responseMessage;
+		logger.info("Server received a SHUTDOWN COMMAND! Stopping now.");
+		System.exit(0);
+		return null;
 	}
 
 	@Override
 	public boolean isValid() {
-		if (serverContext.getServerStatus() != ServerStatusType.IDLE) {
-			return true;
-		}
-		return false;
+		return true;
 	}
 
 }
