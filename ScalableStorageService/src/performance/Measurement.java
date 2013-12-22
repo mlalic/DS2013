1f8aa75f207c94301564fd3563b979589d2ada76
Balsa Bojic <balsha.bojic@gmail.com>
2013-12-22 19:09:50 +0100

    Added new measurement test for constant data set

--- ScalableStorageService/src/performance/Measurement.java
+++ ScalableStorageService/src/performance/Measurement.java
@@ -35,7 +35,15 @@ public class Measurement {
 		//measurement.testFiveServers(1);
 		//measurement.testFiveServers(5);
 		//measurement.testFiveServers(10);
-		measurement.testFiveServers(15);
+		//measurement.testFiveServers(15);
+		
+		/**
+		 * Testing constant data set with three servers, n clients
+		 */
+		//measurement.testThreeServersWithConstantData(1);
+		//measurement.testThreeServersWithConstantData(5);
+		//measurement.testThreeServersWithConstantData(10);
+		measurement.testThreeServersWithConstantData(15);
 		
 	}
 	
@@ -51,7 +59,7 @@ public class Measurement {
 		List<ClientThread> threads = new LinkedList<ClientThread>(); 		
 		for (int i = 0; i < n; i++) {
 			int scope = i * 10000;
-			ClientThread client = new ClientThread("127.0.0.1", 50000, scope, "put");
+			ClientThread client = new ClientThread("127.0.0.1", 50000, scope, "put", 10000);
 			threads.add(client);
 			client.start();
 		}
@@ -84,7 +92,7 @@ public class Measurement {
 		for (int i = 0; i < n; i++) {
  			int scope = i * 10000;
 			int port = 50000  + (i % 3);
-			ClientThread client = new ClientThread("127.0.0.1", port, scope, "put");
+			ClientThread client = new ClientThread("127.0.0.1", port, scope, "put", 10000);
 			threads.add(client);
 			client.start();
 		}
@@ -116,7 +124,7 @@ public class Measurement {
 		for (int i = 0; i < n; i++) {
  			int scope = i * 10000;
 			int port = 50000  + (i % 5);
-			ClientThread client = new ClientThread("127.0.0.1", port, scope, "put");
+			ClientThread client = new ClientThread("127.0.0.1", port, scope, "put", 10000);
 			threads.add(client);
 			client.start();
 		}
@@ -133,6 +141,39 @@ public class Measurement {
 		endTime = System.currentTimeMillis();	
 		
 		System.out.println("Time for executing testFiveServers (5 servers, " + n + " clients, 10000 data) is: " + (endTime - startTime)/1000 + " seconds ");
+	}
+	
+	/**
+	 * Testing constant data set with three servers, n clients
+	 */
+	public void testThreeServersWithConstantData(int n) {
+		long startTime;
+		long endTime;
+		
+		startTime = System.currentTimeMillis();
+		
+		List<ClientThread> threads = new LinkedList<ClientThread>(); 
+		int data = 1000000 / n;
+		for (int i = 0; i < n; i++) {
+ 			int scope = i * data;
+			int port = 50000  + (i % 3);
+			ClientThread client = new ClientThread("127.0.0.1", port, scope, "put", data);
+			threads.add(client);
+			client.start();
+		}
+		
+		for (ClientThread client : threads) {
+			try {
+				client.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		endTime = System.currentTimeMillis();	
+		
+		System.out.println("Time for executing testThreeServersWithConstantData (3 servers, " + n + " clients, 10000 data) is: " + (endTime - startTime)/1000 + " seconds ");
 	}
 	
 }
