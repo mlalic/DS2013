8a2a2d0f69cd392b2b4365f411659ec77f31c717
Marko Lalic <marko.lalic@gmail.com>
2013-12-29 04:16:11 +0100

    Add a new server-side command: update replicated data.
    When the server receives this command, it stores the given data
    in a local key/value store as a replication of the master server.

--- ScalableStorageService/src/app_kvEcs/communication/kvMessageBuilder.java
+++ ScalableStorageService/src/app_kvEcs/communication/kvMessageBuilder.java
@@ -1,8 +1,12 @@
 package app_kvEcs.communication;
 
+import java.util.Map;
+
 import com.google.gson.Gson;
+
 import common.messages.KVMessage;
 import common.messages.KVMessageImpl;
+import common.messages.KVMessage.StatusType;
 import common.metadata.MetaData;
 import common.metadata.MetaDataTransport;
 import common.metadata.ServerNode;
@@ -40,5 +44,11 @@ public class kvMessageBuilder {
     	Gson gson = new Gson();
     	String destinationNodeJson = gson.toJson(destinationNode);
         return new KVMessageImpl(KVMessage.StatusType.MOVE_DATA, null, destinationNodeJson);
+    }
+    
+    public static KVMessage buildUpdateReplicatedDataMessage(Map<String, String> data) {
+    	Gson gson = new Gson();
+    	String jsonData = gson.toJson(data);
+    	return new KVMessageImpl(StatusType.UPDATE_REPLICATED_DATA, null, jsonData);
     }
 }
